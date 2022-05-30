b024d942aef34b72d61a66541a3b1581c12c0671
cody-ferguson <codylee1011@hotmail.com>
2022-05-30 13:02:36 -0500

    New name: NodejsEvents

---
+++ Sources/NodejsEvents/Events.swift
@@ -0,0 +1,34 @@
+private typealias function = (Any) -> Void
+private struct Event {
+    var name: String
+    var function: function?
+}
+extension Event {
+  static func == (left: Event, right: String) -> Bool {
+    return left.name == right
+  }
+}
+
+public class EventEmitter {
+    private var events: [Event] = []
+    init(_ Initialevents: [String]){
+        for event in Initialevents {
+            events.append(Event(name: event)
+        }
+    }
+    public func emit(_ event: String,_ args: Any = ""){
+        for currentEvent in events {
+            if currentEvent == event {
+                currentEvent.function(args)
+            }
+        }
+    }
+    public func on(_ eventName: String, _ callback: @escaping function){
+        for i in 0...events.count-1 {
+            let event = events[i]
+            if event == eventName {
+                events[i] = Event(name: eventName, function: callback)
+            }
+        }
+    }
+}
--- Tests/EventsTests/EventsTests.swift
+++ Tests/EventsTests/EventsTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import Events
+@testable import NodejsEvents
 
 final class EventsTests: XCTestCase {
     func testExample() throws {
